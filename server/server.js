461c743585b306490443e72bc39048c8b7f4ce7a
summitsetia <summitsetia@gmail.com>
2025-01-27 15:58:10 +1300

    Started Navbar (Before Implementing Routing)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const corsOptions = {
+  orgin: ["htttp://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
